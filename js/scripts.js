97310476e9862ec0932a338424b209c9b1956b5c
Minh Khang Nguyen <46115541+mkhangg@users.noreply.github.com>
2021-05-11 15:25:51 +0700

    commit 6

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,22 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+var checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
